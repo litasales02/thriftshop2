8a67f2ff73e7074684c7fd085ee37d948b73d03e
jhonortes <jhonortes@yahoo.com>
2019-02-15 07:07:40 +0800

    error

--- index.php
+++ index.php
@@ -190,7 +190,7 @@ switch (ENVIRONMENT)
  * ---------------------------------------------------------------
  */
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__.'\vendor\autoload.php';
 use Kreait\Firebase\Factory;
  
 use Kreait\Firebase\ServiceAccount;
